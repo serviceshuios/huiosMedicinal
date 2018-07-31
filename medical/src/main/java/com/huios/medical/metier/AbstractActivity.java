719d006932e3ed4e2b3a87e3f6a96fcef8bb675d
HUIOS <services.huios@gmail.com>
2018-07-31 22:50:23 +0200

    classe AbstractActivity

---
+++ medical/src/main/java/com/huios/medical/metier/AbstractActivity.java
@@ -0,0 +1,71 @@
+package com.huios.medical.metier;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Version;
+
+public class AbstractActivity {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO) 
+	protected Long id;
+	@Version
+	protected Long version;
+	
+	
+	public AbstractActivity(Long id, Long version) {
+		super();
+		this.id = id;
+		this.version = version;
+	}
+	
+	public AbstractActivity() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public Long getVersion() {
+		return version;
+	}
+	public void setVersion(Long version) {
+		this.version = version;
+	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		result = prime * result + ((version == null) ? 0 : version.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		AbstractActivity other = (AbstractActivity) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		if (version == null) {
+			if (other.version != null)
+				return false;
+		} else if (!version.equals(other.version))
+			return false;
+		return true;
+	}
+	
+	
+}
